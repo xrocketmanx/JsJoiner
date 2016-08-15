3cb079884c67001d29df7351a7a496549872d667
Alex <xrocketmanx007@gmail.com>
2016-08-15 22:48:12 +0300

    add user configuration file capability

--- file-manager.js
+++ file-manager.js
@@ -4,14 +4,16 @@ var config = require('./config');
 
 module.exports = function(dir) {
 	var ignoredFiles = getIgnoredFiles();
+	var userConfig = getUserConfig();
+	var jsPath = userConfig.js;
 	var self = this;
 
 	this.watch = function(callback) {
-		fs.watch(dir, callback);
+		fs.watch(jsPath, callback);
 	};
 
 	this.getWatchedFiles = function() {
-		return fs.readdirSync(dir).filter(function(file) {
+		return fs.readdirSync(jsPath).filter(function(file) {
 		    return self.isWatchFile(file);
 		});
 	};
@@ -22,13 +24,22 @@ module.exports = function(dir) {
 	        buf += fs.readFileSync(getPath(files[i]));
 	    }
 
-	    fs.writeFileSync(dir + config.resultFileName, minify ? minifier.js(buf) : buf);
+	    fs.writeFileSync(jsPath + config.resultFileName, minify ? minifier.js(buf) : buf);
 	}.delay(100);
 
 	this.isWatchFile = function(file) {
 	    return file.indexOf(".js") === file.length - 3 && 
 	        ignoredFiles.indexOf(file) === -1;
 	};
+
+
+	function getUserConfig() {
+		var path = dir + config.userConfig;
+		if (fs.existsSync(path)) {
+			var userConfig = JSON.parse(fs.readFileSync(path));
+		}
+		return userConfig;
+	}
 
 	function getIgnoredFiles() {
 	    var ignoreFilePath = getPath(config.ignoreFileName);
@@ -43,6 +54,6 @@ module.exports = function(dir) {
 	}
 
 	function getPath(file) { 
-		return dir + file; 
+		return jsPath + file; 
 	}
 }
