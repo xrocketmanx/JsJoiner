47aa41110feeff172b1e69a7c3fff71666ff329c
Alex <xrocketmanx007@gmail.com>
2016-08-18 14:56:06 +0300

    rename fileManager class to fileJoiner

--- controllers/file-joiner.js
+++ controllers/file-joiner.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 var path = require('path');
 
-function FileManager(dir, config) {
+function FileJoiner(dir, config) {
 	this.dir = dir;
 	this.config = config;
 	//this.files = this.getWatchedFiles(this.dir); 
 }
 
-FileManager.prototype.watch = function(minify) {
+FileJoiner.prototype.watch = function(minify) {
 	var self = this;
 	fs.watch(self.dir, function(event, file) {
 		if (event === "rename" || event == "delete") {
@@ -26,24 +26,24 @@ this.joinFiles = function(minify) {
 	//abstract
 };
 
-FileManager.prototype.getWatchedFiles = function(dir) {
+FileJoiner.prototype.getWatchedFiles = function(dir) {
 	var self = this;
 	return fs.readdirSync(dir).filter(function(file) {
 	    return self.isWatchFile(file);
 	});
 };
 
-FileManager.prototype.isWatchFile = function(file) {
+FileJoiner.prototype.isWatchFile = function(file) {
 	//virtual
     return file.indexOf(this.config.ext) === file.length - this.config.ext.length;
 };
 
-FileManager.prototype.writeResult = function(result) {
+FileJoiner.prototype.writeResult = function(result) {
 	fs.writeFileSync(this.dir + this.config.resultFileName, result);
 };
 
-FileManager.prototype.getPath = function(file) { 
+FileJoiner.prototype.getPath = function(file) { 
 	return path.join(this.dir, file); 
 };
 
-module.exports = FileManager;
+module.exports = FileJoiner;
