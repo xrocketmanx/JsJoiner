118de7e656b234bf0ae44a5bda4c1a7a7651c9a1
Alex <xrocketmanx007@gmail.com>
2016-08-21 17:49:44 +0300

    fix user config path to be relative to proj dir

--- config-manager.js
+++ config-manager.js
@@ -4,5 +4,9 @@ var fs = require('fs');
 module.exports = function(dir, config) {
 	dir = dir || config.dir;
 	var configPath = path.join(dir, config.userConfig);
-	return JSON.parse(fs.readFileSync(configPath));
+	var userConfig = JSON.parse(fs.readFileSync(configPath));
+	for (var key in userConfig) {
+		userConfig[key] = path.join(dir, userConfig[key]);
+	}
+	return userConfig;
 };
